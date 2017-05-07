922fc4cc6619eb4f1160e3bf6db5c3d129fa7107
Ilyas Git <ilyasridhuan@gmail.com>
2017-05-07 12:33:03 +0800

    First Commit/Added AWS implementation for DynamoDB and SQS

---
+++ src/AWS/DynamoDBHandler.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module AWS.DynamoDBHandler where
+
+import           Control.Lens
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.AWS
+import           Data.ByteString         (ByteString)
+import           Data.Conduit
+import qualified Data.Conduit.List       as CL
+import           Data.HashMap.Strict     (HashMap)
+import qualified Data.HashMap.Strict     as Map
+import           Data.Monoid
+import           Data.Text               (Text)
+import qualified Data.Text               as Text
+import qualified Data.Text.IO            as Text
+import           Network.AWS.Data
+import           Network.AWS.DynamoDB
+import           Network.AWS.Auth
+import           System.IO
+
+printTables :: Region -> IO ()
+printTables region = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+
+    runResourceT . runAWST env . within region $ do
+        say $ "Listing all tables in region " <> toText region
+        paginate listTables
+            =$= CL.concatMap (view ltrsTableNames)
+             $$ CL.mapM_ (say . mappend "Table: ")
+
+insertItem :: Region -> Text -> HashMap Text AttributeValue -> IO PutItemResponse
+insertItem region table item = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+
+    runResourceT . runAWST env . within region $ do
+        say $ "Inserting item into table '"
+           <> table
+           <> "' with attribute names: "
+           <> Text.intercalate ", " (Map.keys item)
+        send $ putItem table & piItem .~ item
+
+getEntry :: Region -> Text -> HashMap Text AttributeValue -> IO GetItemResponse
+getEntry region table item = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+
+    runResourceT . runAWST env . within region $ do
+        say $ "Retrieving item from table '"
+            <> table
+            <> "' with keys: "
+            <> Text.intercalate ", " (Map.keys item)
+        send $ getItem table & giKey .~ item
+
+updateEntry :: Region -> Text -> HashMap Text AttributeValue -> Text -> HashMap Text AttributeValue -> IO UpdateItemResponse
+updateEntry region table key expr val = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+
+    runResourceT . runAWST env . within region $ do
+        say "Updating "
+        send $ updateItem table & uiKey .~ key & uiUpdateExpression .~ Just expr & uiExpressionAttributeValues .~ val
+
+scanTable :: Region -> Text -> Text -> IO ScanResponse
+scanTable region table scanFilter = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+
+    runResourceT . runAWST env . within region $ do
+        say $ "Scanning items in table "
+        send $ scan table & sFilterExpression .~ Just scanFilter
+
+
+
+say :: MonadIO m => Text -> m ()
+say = liftIO . Text.putStrLn
---
+++ src/AWS/SQSHandler.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+module AWS.SQSHandler where
+
+import           Data.Text               (Text)
+import qualified Data.Text.IO            as Text
+
+import           System.IO
+import Network.AWS.SQS
+import           Network.AWS.Data
+import           Network.AWS.Auth
+import           Control.Monad.Trans.AWS
+import Data.HashMap.Strict
+import Control.Lens
+import           Data.Monoid
+import           Control.Monad.IO.Class
+
+getMessage :: Region -> Text -> [Text] -> IO ReceiveMessageResponse
+getMessage region qUrl msgAttribNames = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+
+    runResourceT . runAWST env . within region $ do
+        say $ "Receiving Message from queue: " <> qUrl
+        send $ receiveMessage qUrl & rmMessageAttributeNames .~ msgAttribNames
+
+delMessage :: Region -> Text -> Text -> IO DeleteMessageResponse
+delMessage region qUrl receiptHandle = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+    runResourceT . runAWST env . within region $ do
+        say $ "Deleting Message from queue: " <> qUrl
+        send $ deleteMessage qUrl receiptHandle
+
+
+
+
+
+say :: MonadIO m => Text -> m ()
+say = liftIO . Text.putStrLn
