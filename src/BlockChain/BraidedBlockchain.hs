090ee71245c9cba3a74e9c27288e961c783c7859
Ilyas Git <ilyasridhuan@gmail.com>
2017-05-08 11:40:13 +0800

    added seeding for ChainData and StateTreeData Tables

--- src/BlockChain/BraidedBlockchain.hs
+++ src/BlockChain/BraidedBlockchain.hs
@@ -7,10 +7,11 @@
 module Blockchain.BraidedBlockchain where
 
 import Crypto.Hash
-import Data.List
+import Data.List (zipWith5)
 import Blockchain.GenAddress
 import Blockchain.Patricia
 import Control.Monad
+import Control.Applicative
 import Control.Monad.Trans
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Base16 as B16
@@ -25,7 +26,7 @@ import GHC.Generics
 import Data.ByteArray (convert)
 import Data.ByteString (ByteString)
 import Data.ByteString.Lazy (toStrict,fromStrict)
-import           Data.HashMap.Strict     (HashMap,insert, (!))
+import           Data.HashMap.Strict     (HashMap,insert, (!),lookup)
 import qualified Data.HashMap.Strict     as Map
 import           Data.Text               (Text,unpack,pack)
 import qualified Data.Text.IO            as Text
@@ -88,26 +89,49 @@ decodeMessage res = do
     -- maybe (deleteMessageResponse) (delMessage NorthVirginia "url here") (head receiptHandle)
     return $ zipWith5 (\a b c d e -> Tx a (AccountAddress b) (TxReceipt c) d e ) validHashes validAddrs receiptHandle cids op
 
-getCIDTree :: Binary a => Hash -> Tx -> MaybeT IO (PatriciaTree a)
-getCIDTree hash Tx {..} = do
-    let treeRoot = Map.singleton "Hash" (attributeValue & avS .~ (Just $ Data.Text.pack $ show hash))
-    res <- liftIO $ getEntry NorthVirginia "MusicStateDB" treeRoot
-    serialBinaryTree <- (MaybeT . return) $ ((res ^. girsItem) ! "Content") ^. avB
-    return $ decode $ fromStrict serialBinaryTree
-
-getBlockchainTree:: Binary a => Hash -> Tx -> MaybeT IO (PatriciaTree a)
-getBlockchainTree hash Tx {..} = do
-    let treeRoot = Map.singleton "Hash" (attributeValue & avS .~ (Just $ Data.Text.pack $ show hash))
-    res <- liftIO $ getEntry NorthVirginia "ChainData" treeRoot
-    serialBinaryTree <- (MaybeT . return) $ ((res ^. girsItem) ! "Content") ^. avB
-    return $ decode $ fromStrict serialBinaryTree
+-- getCIDTree :: (Binary k, Binary v) => Hash -> Tx -> MaybeT IO (PatriciaTree Text Hash)
+-- getCIDTree hash Tx {..} = do
+--     let treeRoot = Map.singleton "Hash" (attributeValue & avS .~ (Just $ Data.Text.pack $ show hash))
+--     res <- liftIO $ getEntry NorthVirginia "MusicStateDB" treeRoot
+--     serialBinaryTree <- (MaybeT . return) $ ((res ^. girsItem) ! "Content") ^. avB
+--     return $ decode $ fromStrict serialBinaryTree
+--
+-- getBlockchainTree:: Binary a => Hash -> Tx -> MaybeT IO (PatriciaTree Text Hash)
+-- getBlockchainTree hash Tx {..} = do
+--     let treeRoot = Map.singleton "Hash" (attributeValue & avS .~ (Just $ Data.Text.pack $ show hash))
+--     res <- liftIO $ getEntry NorthVirginia "ChainData" treeRoot
+--     serialBinaryTree <- (MaybeT . return) $ ((res ^. girsItem) ! "Content") ^. avB
+--     return $ decode $ fromStrict serialBinaryTree
 
 getChainTip :: MaybeT IO Bead
 getChainTip = do
-    let chainTip = Map.singleton "Hash" (attributeValue & avS .~ (Just $ Data.Text.pack "ChainTip"))
+    let chainTip = Map.singleton "ID" (attributeValue & avS .~ (Just $ Data.Text.pack "ChainTip"))
     res <- liftIO $ getEntry NorthVirginia "ChainData" chainTip
     tipNumber <- (MaybeT . return ) $ ((res ^. girsItem) ! "BlockNumber") ^. avS
-    let getBlockTip = Map.singleton "Hash" (attributeValue & avS .~ Just tipNumber)
+    let getBlockTip = Map.singleton "ID" (attributeValue & avS .~ Just tipNumber)
     blockRes <- liftIO $ getEntry NorthVirginia "ChainData" getBlockTip
     blockData <- (MaybeT . return ) $ ((res ^. girsItem) ! "BlockData") ^. avB
     return $ decode $ fromStrict blockData
+
+seedChainData :: IO PutItemResponse
+seedChainData = do
+    let genesisBlock = toStrict $ encode genesisBead
+    let zerothBlock = insert "ID" (attributeValue & avS .~ Just "0") $ insert "BlockData" (attributeValue & avB .~ Just genesisBlock) Map.empty
+    insertItem NorthVirginia "ChainData" zerothBlock
+
+seedStateData :: MaybeT IO PutItemResponse
+seedStateData =  do
+    entries <- liftIO $ scanTable NorthVirginia "Music" "attribute_exists(Address)"
+    entriesList <-  (MaybeT . return ) $ listify (entries ^. srsItems)
+    let pTree = foldr (\x -> flip ( `insertPatriciaTree` fst x) (snd x)) singleton entriesList
+    let encodedTree = toStrict $ encode pTree
+    root <- (MaybeT . return ) $ getRoot pTree
+    let seedTree = insert "Hash" (attributeValue & avS .~ Just (Data.Text.pack $ show root)) $ insert "TreeData" (attributeValue & avB .~ Just encodedTree) Map.empty
+    liftIO $ insertItem NorthVirginia "MusicStateDB" seedTree
+
+listify :: [HashMap Text AttributeValue]  -> Maybe [(String,String)]
+listify = traverse (\x -> do
+    maybeAddress <- Data.Text.unpack <$> (Data.HashMap.Strict.lookup "Address" x >>= flip (^.) avS)
+    maybeHash <- Data.Text.unpack <$> (Data.HashMap.Strict.lookup "Artist" x >>= flip (^.) avS)
+    hash <- (digestFromByteString .fst .B16.decode . B8.pack) maybeHash :: Maybe Hash
+    return (maybeAddress, show hash))
