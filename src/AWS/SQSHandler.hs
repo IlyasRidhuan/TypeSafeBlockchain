2df0b16c202ff4c45c6c6a7150d70b5cd07b193f
Ilyas Git <ilyasridhuan@gmail.com>
2017-05-13 14:20:25 +0800

    added sndMessage

--- src/AWS/SQSHandler.hs
+++ src/AWS/SQSHandler.hs
@@ -21,7 +21,7 @@ getMessage region qUrl msgAttribNames = do
 
     runResourceT . runAWST env . within region $ do
         say $ "Receiving Message from queue: " <> qUrl
-        send $ receiveMessage qUrl & rmMessageAttributeNames .~ msgAttribNames
+        send $ receiveMessage qUrl & rmMessageAttributeNames .~ msgAttribNames & rmWaitTimeSeconds ?~ 20 & rmMaxNumberOfMessages ?~ 10
 
 delMessage :: Region -> Text -> Text -> IO DeleteMessageResponse
 delMessage region qUrl receiptHandle = do
@@ -32,8 +32,13 @@ delMessage region qUrl receiptHandle = do
         send $ deleteMessage qUrl receiptHandle
 
 
-
-
+sndMessage :: Region -> Text -> [Text] -> IO [SendMessageResponse]
+sndMessage region qUrl xs = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv $ FromKeys "AKIAJ5Q7GQRONGS6VGEQ" "nXhTOVeS9qsWDQ9MnecXq8qn6EhoMRvYPgVliMxe"
+    runResourceT . runAWST env . within region $ do
+        say $ "Sending message to queue: " <> qUrl
+        traverse (send . sendMessage qUrl ) xs
 
 say :: MonadIO m => Text -> m ()
 say = liftIO . Text.putStrLn
